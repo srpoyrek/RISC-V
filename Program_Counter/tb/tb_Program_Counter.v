9aa24f220d2bc7895d1915a881c0fb9df2e915aa
Manojna Sistla <sistlamanojnamanu@gmail.com>
2020-06-05 21:56:08 -0500

    Program Counter and control unit files added

---
+++ Program_Counter/tb/tb_Program_Counter.v
@@ -0,0 +1,9 @@
+//Project: RISC-V 32 bit Architecture
+//Module: tb_Program_Counter (FU)
+//Author: Sistla Manojna
+//Updated: 06/03/2020
+//Updates the Program Counter value
+//Updates:
+
+module tb_Program_Counter();
+endmodule
