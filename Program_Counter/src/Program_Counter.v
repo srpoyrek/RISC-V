0dfdfe61adac2123833ae34d921f889a803d3ca4
shreyas poyrekar <shreyaspoyrekar@gmail.com>
2020-06-12 15:21:36 -0500

    pc updates

--- Program_Counter/src/Program_Counter.v
+++ Program_Counter/src/Program_Counter.v
@@ -7,38 +7,29 @@
 //Updates:
 
 
-module program_counter(PC_Value, clk, rst, stall, PCWrite, PCSrc);
+module program_counter(clk, rst, PC_Value, stall, PCWrite, PCSrc);
+
+parameter n_bit = 32; // number of bits for progam counter register
 
 input clk, rst;
-input stall, PCWrite;//control signals stall -> for stall, PCWrite -> for indicating branch
-input[31:0] PCSrc;//branch address
-output reg[31:0] PC_Value;
+input stall, PCWrite; //control signals stall -> for stall, PCWrite -> for indicating branch
+input[n_bits-1 :0] PCSrc; //branch address
+output reg[n_bits-1:0] PC_Value;
 
-always @(posedge clk)
-begin
-
-    if(rst == 0)
-        begin
-            PC_Value <= 0;
+always @(posedge clk) begin
+    if(~rst) begin
+            PC_Value <= 31'd0;
+    end
+    else begin
+    	if(~stall) begin
+            if(PCWrite) begin
+            	PC_Value <= PCSrc;
+            end
+            else begin
+                PC_Value <= PC_Value + 4;
+            end
         end
-    else
-        begin
-            if(stall == 0)
-                begin
-                    if(PCWrite == 1)
-                        begin
-                            PC_Value <= PCSrc;
-                        end
-                    else
-                        begin
-                            PC_Value <= PC_Value + 4;
-                        end
-                end
-            else
-                begin 
-                    PC_Value <= PC_Value;     
-                end
-        end
+    end
 
 end
 
